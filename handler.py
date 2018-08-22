5abf104314b05ee33e36fd089e6fd8e30af8a8ed
hrg921 <hrg921@gmail.com>
2018-08-22 23:54:26 +0900

    delete unnecessary index "message"

--- handler.py
+++ handler.py
@@ -6,13 +6,9 @@ def main(event, context):
     params = event['queryStringParameters']
     result = elastic_search.get(keyword=params["keyword"], size=int(params["size"]), is_suggest=bool(params.get("is_suggested")))
 
-    body = {
-        "message": json.dumps(result)
-    }
-
     response = {
         "statusCode": 200,
-        "body": json.dumps(body)
+        "body": json.dumps(result)
     }
 
     return response
